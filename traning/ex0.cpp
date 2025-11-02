9de30d83a877457c58faa8dd85109c1befc9cde8
youssef el amrani <youssefelamranizt@gmail.com>
2025-11-02 21:33:41 +0100

    finishing ex0

---
+++ traning/ex0.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hp on 02/11/2025.
+//
+
+#include <iostream>
+
+int main() {
+    int a ,b;
+    printf(" etrer deux nembers :");
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    printf("la somme de  %d  et %d = %d",a,b,a+b);
+
+return 0;
+}
